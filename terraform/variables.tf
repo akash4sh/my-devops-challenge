6db9489d2afb460d71e6b32e59ffc204b6f66913
Akash LM <akashnihas@gmail.com>
2026-05-09 19:02:00 +0530

    fix(terraform): remove hardcoded api_token default, mark sensitive

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -12,6 +12,6 @@ variable "memory_quota" {
 
 variable "api_token" {
   type        = string
-  description = "API token consumed by the app"
-  default     = "sk-skybyte-prod-7f3c9a2b1e8d4a6c"
+  description = "API token for the application.Supply via TF_VAR_api_token"
+  sensitive   = true
 }
